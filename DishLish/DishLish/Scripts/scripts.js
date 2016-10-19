a1fea0ad1dd3591df362c3ec6b66fd47961c5111
marinaher <b-her@live.com>
2016-10-18 20:24:10 -0500

    updated migrations and css

--- DishLish/DishLish/Scripts/scripts.js
+++ DishLish/DishLish/Scripts/scripts.js
@@ -14,4 +14,12 @@
 
         direction: "vertical"
     });
+
+    $('#search').on('keyup', function () {
+        var pattern = $(this).val();
+        $('.searchable-container .items').hide();
+        $('.searchable-container .items').filter(function () {
+            return $(this).text().match(new RegExp(pattern, 'i'));
+        }).show();
+    });
 });
