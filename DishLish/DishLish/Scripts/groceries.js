d7013b53584bc0b9fe88596b4cdf49d649dd19d8
marinaher <b-her@live.com>
2016-10-24 14:37:56 -0500

    added groceries script file

---
+++ DishLish/DishLish/Scripts/groceries.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    $(function () {
+        $("#add").click(function () {
+            var input = $('#input').val();
+            var check = $('<button class="checked btn">Check</button>');
+            var newEl = $('<li class="liEl"></li>');
+            if (input === '') return;
+            newEl.append(input);
+            newEl.append(check);
+            $('#list').append(newEl);
+            newEl.addClass("animated bounce");
+            $('#input').val('');
+
+            if ($("#dropDown").val() == "vegetableCategory") {
+                $(newEl).css("background-color", "green");
+            }
+            else if ($("#dropDown").val() == "meatCategory") {
+                $(newEl).css("background-color", "yellow");
+            }
+            else if ($("#dropDown").val() == "grainCategory") {
+                $(newEl).css("background-color", "brown");
+            }
+            else if ($("#dropDown").val() == "dairyCategory") {
+                $(newEl).css("background-color", "pink");
+            }
+            else if ($("#dropDown").val() == "fruitCategory") {
+                $(newEl).css("background-color", "orange");
+            }
+            else if ($("#dropDown").val() == "otherCategory") {
+                $(newEl).css("background-color", "blue");
+            }
+
+            $(check).click(function () {
+                $(this).parent().css('text-decoration', 'line-through');
+
+            });
+        });
+
+        $('#remove').click(function (evt) {
+            var lastEl = $('li.liEl').last();
+            lastEl.remove();
+        });
+
+        $('#input').keypress(function (evt) {
+            if (evt.keyCode === 13) {
+                $("#add").click();
+            }
+        });
+    });
+})
